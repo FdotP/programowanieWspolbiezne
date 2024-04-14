fcd3fd1b84294dfac94657651e5b6d7d5e20262c
Adrian <147884387+venomiakk@users.noreply.github.com>
2024-04-14 22:08:47 +0200

    dodane testy

---
+++ UnitTests/BallFunctionsTest.cs
@@ -0,0 +1,21 @@
+﻿using Dane.model;
+using Logika;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests
+{
+    internal class BallFunctionsTest
+    {
+        [Test]
+        public void constructorTest()
+        {
+            Board board = new Board(1);
+            BallFunctions bf = new BallFunctions();
+            Assert.IsNotNull(bf.generateBalls(board));
+        }
+    }
+}
---
+++ UnitTests/BoardTest.cs
@@ -0,0 +1,32 @@
+﻿using Dane.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests
+{
+    internal class BoardTest
+    {
+        [Test]
+        public void constructorTest()
+        {
+            Board board = new Board(4);
+            Assert.IsNotNull(board);
+        }
+
+        [Test]
+        public void getterTest() {
+            Board board = new Board(4);
+            Assert.That(board.getBalls, Is.Not.Null);
+        }
+        [Test]
+        public void setterTest() {
+            Board board = new Board(4);
+            Ball[] balls = new Ball[5];
+            board.setBalls(balls);
+            Assert.That(board.getBalls(), Is.EqualTo(balls));
+        }
+    }
+}
